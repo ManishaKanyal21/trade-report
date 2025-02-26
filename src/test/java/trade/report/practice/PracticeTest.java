bdeb4fb79aadf8a4241fa074e56189645cf276bb
ManishaKanyal21 <manishajobs21@gmail.com>
2025-02-26 21:50:48 +0000

    Testing git push

---
+++ src/test/java/trade/report/practice/PracticeTest.java
@@ -0,0 +1,17 @@
+package trade.report.practice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class PracticeTest {
+
+    @Test
+    void name() {
+    }
+}
